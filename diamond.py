ce5d8979c6ba9dccfe994c47e5dc5052340e6d97
pujithaGuttala <108428114+pujithaGuttala@users.noreply.github.com>
2022-08-08 16:10:41 +0530

    Add files via upload

---
+++ diamond.py
@@ -0,0 +1,24 @@
+i=1
+while i<=5:
+	b=1
+	while b<=5-i:
+		print(" ",end="")
+		b+=1
+	j=1
+	while j<=i:
+		print("*",end=" ")
+		j+=1
+	print()
+	i+=1
+i=4
+while i>=1:
+	b=1
+	while b<=5-i:
+		print(" ",end="")
+		b+=1
+	j=1
+	while j<=i:
+		print("*",end=" ")
+		j+=1
+	print()
+	i-=1
